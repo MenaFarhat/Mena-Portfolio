98cda604d54db13bfad1c0ccb10b90e0896c32d6
MinaFarhat <Mina.farhat333@gmail.com>
2026-02-19 21:44:46 +0300

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  base: "/"
+  plugins: [react()],
+  base: '/', // مهم جدًا للنشر على Netlify من GitHub
+  build: {
+    outDir: 'dist', // المجلد الافتراضي للنشر
+    emptyOutDir: true, // يمسح مجلد dist قبل كل build
+  },
 })
