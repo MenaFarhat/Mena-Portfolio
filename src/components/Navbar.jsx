4c0344e8f6f3c76de6602d263e7fb300598d00de
MinaFarhat <Mina.farhat333@gmail.com>
2026-02-23 03:42:47 +0300

    update logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,7 +41,7 @@ export default function Navbar() {
           whileTap={{ scale: 0.9 }}
           onClick={() => scrollToSection("home")}
           className="h-9 cursor-pointer"
-          src="assets/MF_navbar.svg"
+          src="assets/MF Navbar.svg"
           alt="Logo"
         />
 
