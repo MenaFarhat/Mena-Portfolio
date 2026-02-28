bd1ec804607f431461c6c79e6d53ef491d0be7dc
MinaFarhat <Mina.farhat333@gmail.com>
2026-02-28 05:57:55 +0300

    Edit the projects links

--- src/experiances.js
+++ src/experiances.js
@@ -6,7 +6,7 @@ export const experiences = [
         period: "Nov 2019 - Aug 2024",
         description:
             "Focused on Software Engineering & Information Systems, completing diverse projects using Java, C++, Flutter, PHP, Python, and more. Developed strong practical programming skills and applied fundamental UI/UX design principles in project work.",
-        logo: "https://MinaFarhat.github.io/Mena-Portfolio/assets/IT-logo.svg",
+        logo: "assets/IT-logo.svg",
     },
     {
         id: 2,
@@ -15,7 +15,7 @@ export const experiences = [
         period: "2022 - Present",
         description:
             "Worked on freelance and training projects developing mobile applications using Flutter. Gained hands-on experience building responsive, user-friendly apps and improved coding practices, problem-solving skills, and full-cycle app development.",
-        logo: "https://MinaFarhat.github.io/Mena-Portfolio/assets/Flutter.svg",
+        logo: "assets/Flutter.svg",
     },
     {
         id: 3,
@@ -24,6 +24,6 @@ export const experiences = [
         period: "2023 - Present",
         description:
             "Worked on freelance and company projects designing interfaces and user experiences. Used Figma to create modern and attractive designs, focusing on usability and turning ideas into practical, user-centered solutions.",
-        logo: "https://MinaFarhat.github.io/Mena-Portfolio/assets/Figma.svg",
+        logo: "assets/Figma.svg",
     },
 ];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -75,7 +75,7 @@ export default function Home() {
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 1, ease: "easeInOut" }}
         >
-          <img className="h-full w-full" src="https://MenaFarhat.github.io/Mena-Portfolio/assets/hero-vector.svg" alt="Hero Vector" />
+          <img className="h-full w-full" src="assets/hero-vector.svg" alt="Hero Vector" />
         </motion.div>
       </div>
     </div>
